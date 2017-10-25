d4631bffffeaaed44de38fbe91331c59e272ba2d
Amaury <amaury@dbertrand.fr>
2017-10-25 18:07:55 +0200

    Ajout de l'effacement des logs précédents et des parties précédentes

--- run_game.sh
+++ run_game.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+read -p "Effacer les logs et parties précédentes ? (y/N)" yesno
+if [[ $yesno =~ [Yy][Ee]?[Ss]? ]]; then
+	rm *.log *.hlt
+fi
+
 ./halite -d "240 160" "python3 MyBot.py" "python3 MyBot.py"
