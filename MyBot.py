3bb51aa167baf18ee11ce58d87eb3fadee894b8f
Dakyne <amaury@dbertrand.fr>
2017-11-16 17:12:59 +0100

    Tuple to debug

--- MyBot.py
+++ MyBot.py
@@ -28,6 +28,7 @@ game = hlt.Game("Dakyne")
 # Then we print our start message to the logs
 logging.info("Starting my newbie bot!")
 
+
 while True:
     # TURN START
     # Update the map for the new turn and get the latest version
@@ -58,45 +59,47 @@ while True:
             logging.debug("else")
             dictVal={}
             for planet in game_map.all_planets():
-                dictVal[planet] = planet.calculate_distance_between(ship)
+                logging.debug(planet)
+                dictVal[planet] = ship.calculate_distance_between(planet)
             listVal = sorted(dictVal.items(), key=lambda x: x[1])
 
             logging.debug("For each planet")
             for planet in listVal:
         #1.2.1. Si la planète est à moi#
                 if planet[0].owner == game_map.get_me():
-        #1.2.1.1. Si pas plein#
+        #1.2.1.1.   Si pas plein#
                     if not planet[0].is_full():
-        #1.2.1.1.1. Si peut dock#
+        #1.2.1.1.1.     Si peut dock#
                         if ship.can_dock(planet[0]):
-        #1.2.1.1.1.1. Dock#
+        #1.2.1.1.1.1.   Dock#
                             logging.debug("1")
                             logging.debug("Planet %s is empty" % planet[0].id)
                             command_queue.append(ship.dock(planet[0]))
                             logging.debug("Ship %s can dock on this planet" % ship.id)
-        #1.2.1.1.2. Si loin#
+        #1.2.1.1.2.     Si loin#
                         else:
-        #1.2.1.1.2.1. Aller à la planète#
+        #1.2.1.1.2.1.   Aller à la planète#
                             logging.debug("2")
                             logging.debug("Move to my planet %s" % planet[0].id)
                             navigate_command = ship.navigate(ship.closest_point_to(planet[0]),game_map,speed=int(hlt.constants.MAX_SPEED),ignore_ships=False)
                         break
-        #1.2.1.2. Si plein#
+        #1.2.1.2.   Si plein#
                     else:
-        #1.2.1.2.1. Planète suivante#
+        #1.2.1.2.1.     Planète suivante#
+                        logging.debug("3")
                         continue
         #1.2.2. Sinon si la planète est à l'ennemi#
                 elif not planet[0].owner == game_map.get_me() and not planet[0].owner == None:
-                    logging.debug("Test")
+                    logging.debug("4")
         #1.2.2.1. Aller à coté des vaisseaux dockés (sans suicide)#
                     dictShip={}
                     for vaisseaux in planet[0].all_docked_ships():
+                        logging.debug(vaisseaux)
                         dictShip[vaisseaux] = vaisseaux.calculate_distance_between(ship)
                     listShip = sorted(dictShip.items(), key=lambda x: x[1])
                     #for vaiss in listShip:
                     #    if not vaiss[0].owner == game_map.get_me():# À priori ce test ne sert à rien
-                    logging.debug("3")
-                    navigate_command = ship.navigate(ship.closest_point_to(vaiss[0]),game_map,speed=int(hlt.constants.MAX_SPEED),ignore_planets=False)
+                    navigate_command = ship.navigate(ship.closest_point_to(listShip[0]),game_map,speed=int(hlt.constants.MAX_SPEED),ignore_planets=False)
                     logging.debug("Move to ship %s" % vaiss[0].id)
                     #        break
                     break
@@ -106,20 +109,23 @@ while True:
         #1.2.3.1. Si peut dock#
                     if ship.can_dock(planet[0]):
         #1.2.3.1.1. Aller se docker#
-                        logging.debug("4")
+                        logging.debug("5")
                         navigate_command = ship.dock(planet[0])
                         logging.debug("Ship %s can dock on this empty planet" % ship.id)
+                        
         #1.2.3.2. Sinon#
                     else:
         #1.2.3.2.1. Se rapprocher puis docker#
+                        logging.debug("6")
                         logging.debug("Moving to empty planet %s" % planet[0].id)
                         navigate_command = ship.navigate(ship.closest_point_to(planet[0]),game_map,speed=int(hlt.constants.MAX_SPEED),ignore_ships=False)
                     break
-                break
+                
 
             if navigate_command:
-                logging.debug("Passer par ici")
+                logging.debug("7")
                 command_queue.append(navigate_command)
+                navigate_command=""
                 
 
 
